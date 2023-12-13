a16b0af6129cf3a382119cab882e28a5c94b0421
robert.drey <robert.drey@questores.com.br>
2023-12-13 13:07:45 -0300

    Finalizado

--- batalhaNaval.vhdl
+++ batalhaNaval.vhdl
@@ -6,8 +6,9 @@ entity batalhaNaval is
         clock : in std_logic;
         reset : in std_logic;
         entradas : in std_logic_vector(3 downto 0);
+        sentido : in std_logic; -- Se for 1 horizontal, se for 0 vertical.
 
-        naviosPosicionados : out std_logic_vector(1 downto 0);
+        naviosPosicionados : out std_logic_vector(2 downto 0);
         vidas: out std_logic_vector(5 downto 0);
         ganhou : out std_logic;
         perdeu : out std_logic;
@@ -20,9 +21,11 @@ architecture arch of batalhaNaval is
     signal y : tipoEstado;
     signal pos_primeiroNavio : std_logic_vector(3 downto 0);
     signal pos_segundoNavio : std_logic_vector(3 downto 0);
+    signal pos_terceiroNavio : std_logic_vector(3 downto 0);
     signal tentativas : integer range 0 to 6 := 0;
     signal primeiroNavioAtingido : std_logic := '0';
     signal segundoNavioAtingido : std_logic := '0';
+    signal terceiroNavioAtingido : std_logic := '0';
     signal codificacao : std_logic_vector(3 downto 0);
 begin
     process(reset, clock)
@@ -56,7 +59,41 @@ begin
                     if entradas /=  pos_primeiroNavio then
                         vidas <= (others => '1');
                         pos_segundoNavio <= entradas;
+                        if sentido = '0' then -- vertical
+                                A := ((not entradas(1) and entradas(0)) or (not entradas(3) and entradas(1) and not entradas(0)) or (not entradas(3) and not entradas(2) and not entradas(0)) or (entradas(3) and entradas(2) and not entradas(1)));
+
+                                B := ((not entradas(3) and entradas(1)) or (entradas(3) and not entradas(1) and entradas(0)) or (entradas(3) and entradas(2) and entradas(0)) or (not entradas(3) and entradas(2) and not entradas(0)));
+
+                                C := ((entradas(3) and not entradas(1) and not entradas(0)) or (entradas(2) and entradas(1) and entradas(0)) or (entradas(3) and entradas(1) and entradas(0)) or (entradas(3) and entradas(2) and not entradas(1)) or (not entradas(3) and not entradas(2) and not entradas(1) and entradas(0)) or (not entradas(3) and not entradas(2) and entradas(1) and not entradas(0)));
+
+                                D := ((entradas(3) and entradas(2) and entradas(0)) or (entradas(2) and entradas(1) and not entradas(0)) or (not entradas(2) and not entradas(1) and not entradas(0)) or (not entradas(3) and entradas(2) and not entradas(0)) or (not entradas(3) and not entradas(2) and not entradas(1)));
+
+                                teste(3) <= A;
+                                teste(2) <= B;
+                                teste(1) <= C;
+                                teste(0) <= D;
+
+                                pos_terceiroNavio(3) <= A;
+                                pos_terceiroNavio(2) <= B;
+                                pos_terceiroNavio(1) <= C;
+                                pos_terceiroNavio(0) <= D;
+                        else -- horizontal
+
+                                A := ((entradas(2) and not entradas(1) and entradas(0)) or (not entradas(2) and not entradas(1) and not entradas(0)) or (entradas(3) and entradas(2) and entradas(1)) or (not entradas(3) and not entradas(2) and entradas(1)) or (not entradas(3) and entradas(1) and not entradas(0)));
+
+                                B := ((not entradas(3) and not entradas(1) and entradas(0)) or (entradas(3) and entradas(2) and not entradas(1)) or (not entradas(2) and entradas(1) and not entradas(0)) or (entradas(3) and not entradas(2) and entradas(1)) or (entradas(3) and entradas(2) and not entradas(0)));
+
+                                C := ((not entradas(2) and entradas(0)) or (not entradas(3) and entradas(2) and not entradas(1)) or (entradas(3) and entradas(2) and entradas(1)));
+
+                                D := ((entradas(1) and not entradas(0)) or (not entradas(2) and not entradas(1) and entradas(0)) or (not entradas(3) and not entradas(2) and entradas(0)));
+
+                                pos_terceiroNavio(3) <= A;
+                                pos_terceiroNavio(2) <= B;
+                                pos_terceiroNavio(1) <= C;
+                                pos_terceiroNavio(0) <= D;
+                        end if;
                         naviosPosicionados(1) <= '1';
+                        naviosPosicionados(2) <= '1';
                         y <= jogadas;
                     else
                         y <= segundoNavio;
@@ -65,16 +102,11 @@ begin
 
                     A := ((not entradas(3) and not entradas(2) and not entradas(1)) or (not entradas(2) and entradas(0)) or (not entradas(3) and entradas(2) and entradas(1) and not entradas(0)) or ((entradas(3) and entradas(2)) and not entradas(1)));
 
-                    B := ((not entradas(1) and not entradas(0)) or ((not entradas(3) and not entradas(2)) and entradas(1)) or ((entradas(3) and entradas(2)) and not entradas(1)) or ((entradas(3) and entradas(2)) and not entradas(0))) ;
+                    B := ((not entradas(1) and not entradas(0)) or ((not entradas(3) and not entradas(2)) and entradas(1)) or ((entradas(3) and entradas(2)) and not entradas(1)) or ((entradas(3) and entradas(2)) and not entradas(0)));
 
                     C := ((not entradas(3) and not entradas(1) and not entradas(0)) or (not entradas(2) and not entradas(1) and not entradas(0)) or ((not entradas(3) and not entradas(2)) and entradas(0)) or (entradas(3) and (not entradas(2) and not entradas(0))) or (entradas(3) and entradas(1) and entradas(0)));
 
                     D := ((not entradas(3) and not entradas(2) and not entradas(0)) or ((not entradas(2) and not entradas(0)) and entradas(1)) or (entradas(2) and (not entradas(1) and not entradas(0))) or (not entradas(3) and (entradas(2) and entradas(1) and entradas(0))) or ((entradas(3) and entradas(0)) and not entradas(2)));
-                     
-                    teste(3) <= A;
-                    teste(2) <= B;
-                    teste(1) <= C;
-                    teste(0) <= D;
 
                     if (pos_primeiroNavio(3) = A and pos_primeiroNavio(2) = B and pos_primeiroNavio(1) = C and pos_primeiroNavio(0) = D) then
                         primeiroNavioAtingido <= '1';
@@ -82,6 +114,9 @@ begin
                     elsif (pos_segundoNavio(3) = A and pos_segundoNavio(2) = B and pos_segundoNavio(1) = C and pos_segundoNavio(0) = D) then
                         segundoNavioAtingido <= '1';
                         naviosPosicionados(1) <= '0';
+                    elsif (pos_terceiroNavio(3) = A and pos_terceiroNavio(2) = B and pos_terceiroNavio(1) = C and pos_terceiroNavio(0) = D) then
+                        terceiroNavioAtingido <= '1';
+                        naviosPosicionados(2) <= '0';
                     else 
                         tentativas <= tentativas + 1; -- Incrementa o contador de tentativas
                             if tentativas < 6 then
